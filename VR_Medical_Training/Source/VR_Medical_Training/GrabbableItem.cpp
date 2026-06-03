503b447e3b56e59d679e60246b725be0581861fa
Alex Wojtowicz <>
2026-06-03 11:04:56 +0200

    test

--- VR_Medical_Training/Source/VR_Medical_Training/GrabbableItem.cpp
+++ VR_Medical_Training/Source/VR_Medical_Training/GrabbableItem.cpp
@@ -36,6 +36,8 @@ void AGrabbableItem::Grab(AVR_Hand_Tracked* Hand)
 	if (OwningHand)
 		return;
 	
+	DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+	
 	OwningHand = Hand;
 	Collider->SetSimulatePhysics(false);
 	if (UseMeshOffsetOnGrab) SetActorRelativeLocation(MeshOffset);
